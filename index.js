e71686540c0c845b6b36752d814426662fa7115a
Lana Worobec <llama.p0rtk3y@gmail.com>
2020-02-01 19:26:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 // Add your functions here
-const map = function(arr, fn){
-    let newArr = []
-    for(const el of arr) {
-        newArr.push(fn(el))
-    }
-    return newArr
+function map(arr, fn){
+  let newArr = []
+  forEach(el => newArr.push(fn(el))
+    return newArr;
 }
